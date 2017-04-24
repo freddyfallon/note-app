c14d4bee24db75d1b98e048f2f914966178dfc5b
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-04-24 16:01:41 +0100

    First test draft

---
+++ spec/note-modelSpec.js
@@ -0,0 +1,12 @@
+(function(exports) {
+  function testNoteQuoteToExist() {
+    var note = new Note();
+    note1 = "Hello";
+    inputNote(note1);
+    saveNote();
+    if (this.noteLog.includes()) {
+       return("Working!");
+        }
+     }
+  testNoteQuoteToExist();
+  })(this);
---
+++ src/note-model.js
@@ -0,0 +1,7 @@
+function Note() {
+  this.noteLog = [];
+
+Note.prototype.inputNote = function(note) {
+  var newNote = note;
+};
+}
