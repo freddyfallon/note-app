d90eec7f8f0571842adf1ef4a74ab411c99e7135
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-04-25 15:51:14 +0100

    Added controller

---
+++ src/index.js
@@ -0,0 +1,4 @@
+list = new NoteList();
+controller = new NoteController(list);
+controller.addNote("Favourite drink: seltzer");
+controller.sendToIndex();
--- src/note-controller.js
+++ src/note-controller.js
@@ -1,1 +1,16 @@
-document.getElementById('app').innerHTML = "Howdy!";
+(function(exports) {
+  function NoteController(noteList) {
+    this.noteListView = new NoteListView(noteList);
+  }
+
+NoteController.prototype.addNote = function(text) {
+  this.noteListView.noteList.createNote(text);
+};
+
+NoteController.prototype.sendToIndex = function() {
+  document.getElementById('note-list').innerHTML = this.noteListView.returnHTMLString();
+};
+
+exports.NoteController = NoteController;
+
+})(this);
